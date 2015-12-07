05d4abaae5af01bdb44d7fd236b95ca3fb3c9a13
Anton Kuznetsov <antonk@andrew.cmu.edu>
2015-12-06 21:49:55 -0500

    Initial Commit

---
+++ SaveSpot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveSpot
+//
+//  Created by Anton Kuznetsov on 12/6/15.
+//  Copyright © 2015 Anton Kuznetsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
