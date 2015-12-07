1feea7824fd09c85a8264088ac60436d9cdd07d7
Anton Kuznetsov <antonk@andrew.cmu.edu>
2015-12-07 17:03:18 -0500

    working directions

--- SaveSpot/ShowLocationVC.swift
+++ SaveSpot/ShowLocationVC.swift
@@ -12,6 +12,8 @@ import CoreLocation
 
 class ShowLocationVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
+    var destination : MKMapItem = MKMapItem()
+    
     // This value may be passed by `HistoryTVController` in `prepareForSegue(_:sender:)`
     var historySpot: Spot?
     
@@ -23,6 +25,13 @@ class ShowLocationVC: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     }
     
     @IBOutlet weak var mapView: MKMapView!
+    func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer{
+        let draw = MKPolylineRenderer(overlay: overlay)
+        draw.strokeColor = UIColor.purpleColor()
+        draw.lineWidth = 3.0
+        print("WASSSSUSUUUP")
+        return draw
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,5 +63,37 @@ class ShowLocationVC: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         mapView.addAnnotation(annotation)
         mapView.setRegion(region,animated:true)
         myPosition = locationMgr.location!.coordinate
+        
+         //create a place mark and a map item
+        let placeMark = MKPlacemark(coordinate: center, addressDictionary: nil)
+        
+         //This is needed when we need to get direction
+        destination = MKMapItem(placemark: placeMark)
+        
+        let request = MKDirectionsRequest()
+        request.source = MKMapItem.mapItemForCurrentLocation()
+        request.destination = destination
+        request.requestsAlternateRoutes = false
+        
+        let directions = MKDirections(request: request)
+        directions.calculateDirectionsWithCompletionHandler{
+            response, error in
+            guard let response = response else {
+                //handle the error here
+                return
+            }
+            let overlays = self.mapView.overlays
+            self.mapView.removeOverlays(overlays)
+            
+            for route in response.routes {
+                
+                self.mapView.addOverlay(route.polyline,
+                    level: MKOverlayLevel.AboveRoads)
+                
+                for next  in route.steps {
+                    print(next.instructions)
+                }
+            }
+        }
     }
 }
